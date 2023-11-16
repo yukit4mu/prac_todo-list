80b31d0a10eb235c316395506c4db7f546f32773
YuaKitamura-230730 <yua_kitamura@decogn.com>
2023-11-16 17:34:10 +0900

    maybe finished my-work

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Person;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/softdelete', function () {
+    Person::find(1)->delete();
 });
